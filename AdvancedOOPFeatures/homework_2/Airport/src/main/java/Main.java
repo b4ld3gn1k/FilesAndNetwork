4e8165dd633a5b5518cc939992f1c06f68fc33bb
arkadii_strelbitskii <foxyincube@icloud.com>
2022-12-04 20:09:04 +0600

    Задание 13.14

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,26 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+import com.skillbox.airport.Terminal;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        findPlanesLeavingInTheNextTwoHours(Airport.getInstance()).forEach(System.out::println);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        return airport.getTerminals().stream()
+                .map(Terminal::getFlights)
+                .flatMap(List::stream)
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE
+                        && flight.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().isBefore(LocalDateTime.now(ZoneId.systemDefault()).plusHours(2))
+                        && flight.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime().isAfter(LocalDateTime.now()))
+                .collect(Collectors.toList());
     }
 
 }
